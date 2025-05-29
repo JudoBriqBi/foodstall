07b9da7ff6c9b4838f3bb152881c5a34cc64afee
JudoBriqbi <judhisthir@briqbi.com>
2025-05-30 03:31:50 +0530

    build attempt#1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
+  plugins: [
+    react(),
+    tailwindcss(),
+    nodePolyfills() // Add polyfills for Node.js modules
   ],
   base: '/',
+  build: {
+    target: 'esnext', // Ensure compatibility with modern environments
+  },
 })
